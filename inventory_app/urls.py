12516ef4063fc986bf09f65612cd225ce70832c5
Your_Name <joseph161216@gmail.com>
2020-08-09 21:39:28 -0700

    crud for category

--- inventory_app/urls.py
+++ inventory_app/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("categories/", views.allCategories, name="inven-categories"),
+  path("category-list/", views.categoryList, name="category-list"),
+  path("category-detail/<str:pk>/", views.categoryDetail, name="category-detail"),
+  path("category-create/", views.categoryCreate, name="category-create"),
+  path("category-update/<str:pk>/", views.categoryUpdate, name="category-update"),
+  path("category-delete/<str:pk>/", views.categoryDelete, name="category-delete"),
   path("items/", views.allItems, name="inven-items"),
   path("users/", views.allUsers, name="inven-users"),
 ]
--- inventory_app/views.py
+++ inventory_app/views.py
@@ -8,12 +8,47 @@ from .models import (Category, Item, User)
 
 # * should seprate into different views
 
-# Get all categories
+# get all categories
 @api_view(["GET"])
-def allCategories(request):
-  categories = Category.objects.all()
+def categoryList(request):
+  categories = Category.objects.all().order_by("id")
   serializer = CategorySerializer(categories, many=True)
   return Response(serializer.data)
+
+# get one category
+@api_view(["GET"])
+def categoryDetail(request, pk):
+  category = Category.objects.get(id=pk)
+  serializer = CategorySerializer(category, many=False)
+  return Response(serializer.data)
+
+# create a category
+@api_view(["POST"])
+def categoryCreate(request):
+  serializer = CategorySerializer(data=request.data)
+
+  if serializer.is_valid():
+    serializer.save()
+
+  return Response(serializer.data)
+
+# update a category
+@api_view(["POST"])
+def categoryUpdate(request, pk):
+  categories = Category.objects.get(id=pk)
+  serializer = CategorySerializer(instance=categories, data=request.data)
+
+  if serializer.is_valid():
+    serializer.save()
+
+  return Response(serializer.data)
+
+# delete one category
+@api_view(["DELETE"])
+def categoryDelete(request, pk):
+  category = Category.objects.get(id=pk)
+  category.delete()
+  return Response("Category successfully deleted!")
 
 # Get all items
 @api_view(["GET"])
